1996063fe3687b9167ab24ca1bad9a9566f23969
Maeda Hanafi <my_pink_han@yahoo.com>
2015-12-09 23:08:41 -0500

    no message

---
+++ zoyage_search.sql
@@ -0,0 +1,21 @@
+﻿SELECT 
+*
+FROM
+	(SELECT 
+		* 
+	FROM 
+		public."PeopleTag", 
+		public."Person",
+		public."Tag",
+		public."TravelInfo"
+	WHERE 
+		"Person"."PersonID" = "PeopleTag"."PeopleID" AND
+		"Tag"."TagID" = "PeopleTag"."TagID" AND
+		"TravelInfo"."PersonID" = "PeopleTag"."PeopleID"
+	) n1
+	 LEFT JOIN "Location"
+	ON "Location"."LocationID" = n1."FromLocation"
+/*,
+	public."TravelInfo"
+WHERE 
+	"TravelInfo"."PersonID" = "n1"."PersonID"*/;
---
+++ zoyage_search2.sql
@@ -0,0 +1,38 @@
+﻿SELECT 
+  * 
+FROM (
+  SELECT 
+    * 
+  FROM
+    public."PeopleTag",
+    public."Tag"
+  WHERE 
+    "PeopleTag"."TagID" = "Tag"."TagID"
+    AND "Tag"."TagName" = 'Cat Lovers'
+ ) tagtbl,
+(
+  SELECT
+   *
+  FROM
+    public."TravelInfo",
+    /*public."Location",*/
+    (
+	select "Location"."LocationID" as fromlocationid from public."Location", public."TravelInfo" 
+	where "Location"."LocationID"="TravelInfo"."FromLocation" 
+	and "Location"."Location"='Kuala Lumpur, Malaysisa'
+    ) fromlocation,
+    (
+	select "Location"."LocationID" as tolocationid from public."Location", public."TravelInfo" 
+	where "Location"."LocationID"="TravelInfo"."ToLocation" 
+	and "Location"."Location"='France'
+    ) tolocation
+  WHERE
+    /*("TravelInfo"."ToLocation" = "Location"."LocationID" OR
+     "TravelInfo"."FromLocation" = "Location"."LocationID" ) AND */
+     ("TravelInfo"."FromLocation"=fromlocationid OR
+     "TravelInfo"."ToLocation"=tolocationid)
+) locationdetailtbl,
+public."Person"  
+   WHERE
+     tagtbl."PeopleID" = "Person"."PersonID"
+     AND locationdetailtbl."PersonID" = "Person"."PersonID";
